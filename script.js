32010db53c6af8ca044648dc7c2503eb994cc638
yongchengzhu <yongchengzhu002@gmail.com>
2023-11-11 22:38:53 -0500

    add cors proxy.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const fetchImages = async () => {
   document.querySelectorAll('img').forEach(image => image.remove());
-  new DOMParser().parseFromString(await (await fetch(document.getElementById('url').value)).text(), 'text/html').querySelectorAll('img').forEach(image => document.body.appendChild(Object.assign(document.createElement('img'), { src: image.src })));
+  new DOMParser().parseFromString(await (await fetch('https://cors-anywhere.herokuapp.com/' + document.getElementById('url').value)).text(), 'text/html').querySelectorAll('img').forEach(image => document.body.appendChild(Object.assign(document.createElement('img'), { src: image.src })));
 }
